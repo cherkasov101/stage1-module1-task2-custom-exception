d022073f39c437d7a50c20aa891a236fd3254575
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-08-12 12:32:34 +0300

    ex t2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,12 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws MyException {
+    if (studentID >= 1 && studentID <= 10) {
+      return Student.getValueOf(studentID);
+    } else {
+      throw new MyException(studentID);
+    }
   }
 
   public static void main(String[] args) {
